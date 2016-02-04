8eb0c51e5fb61cb3ba30ca50ac251ece4eedc211
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:17:40 -0700

    2-4-2016

---
+++ reversing_a_list.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 12:12:48 2016
+
+@author: Geetha Yedida
+"""
+
+my_list = range(1, 11)
+
+# Add your code below!
+my_list = my_list[::-1]
+backwards =my_list[::-1]
+print backwards
