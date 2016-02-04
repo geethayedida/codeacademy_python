d045c9e4f5a62f5f0b2d3c231491735e9475fb48
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:19:37 -0700

    2-4-2016

---
+++ threes_and_fives_list_comprehension.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 12:42:51 2016
+
+@author: Geetha Yedida
+"""
+
+threes_and_fives = [x for x in range(1,16) if x%3 == 0 or x% 5 == 0]
+print threes_and_fives
