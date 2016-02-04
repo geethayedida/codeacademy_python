606a7e98fba045977de1e164ed0ffa58963d0851
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:19:57 -0700

    2-4-2016

---
+++ list_slicing.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 12:52:56 2016
+
+@author: Geetha Yedida
+"""
+
+garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
+
+message = garbled[::-2]
+print message
