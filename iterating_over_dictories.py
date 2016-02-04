657cbd9e4983c3002a5679b96d5e5498a3a45301
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:19:17 -0700

    2-4-2016

---
+++ iterating_over_dictories.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 12:41:22 2016
+
+@author: Geetha Yedida
+"""
+
+movies = {
+	"Monty Python and the Holy Grail": "Great",
+	"Monty Python's Life of Brian": "Good",
+	"Monty Python's Meaning of Life": "Okay"
+}
+print movies.items()
