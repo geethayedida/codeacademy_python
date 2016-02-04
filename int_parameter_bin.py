de2bbc5e11316aabbba1813873ea467471d9f389
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:21:11 -0700

    2-4-2016

---
+++ int_parameter_bin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 14:19:36 2016
+
+@author: Geetha Yedida
+"""
+
+print int("1",2)
+print int("10",2)
+print int("111",2)
+print int("0b100",2)
+print int(bin(5),2)
+# Print out the decimal equivalent of the binary 11001001.
+print int("11001001",2)
