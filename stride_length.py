56ded0bcffa6e9bbb5df403e45579e392b919286
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:18:03 -0700

    2-4-2016

---
+++ stride_length.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 12:20:59 2016
+
+@author: Geetha Yedida
+"""
+
+to_one_hundred = range(101)
+# Add your code below!
+
+backwards_by_tens = to_one_hundred[::10]
+print backwards_by_tens
