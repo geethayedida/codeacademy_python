471bba2d873efeb78e7e553c3127b3a643e38863
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:18:24 -0700

    2-4-2016

---
+++ more_slicing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 12:21:50 2016
+
+@author: Geetha Yedida
+"""
+
+to_21 = range(1,22)
+odds = to_21[::2]
+middle_third = to_21[7:14:1]
+
+print to_21
+print odds
+print middle_third
