b6aa9d4656c8b4ac4ba55dbb2f1eaf9a42054d67
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:17:03 -0700

    2-4-2016

---
+++ omiting_indices.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 11:55:02 2016
+
+@author: Geetha Yedida
+"""
+
+my_list = range(1, 11) # List of numbers 1 - 10
+
+# Add your code below!
+print my_list[::2]
