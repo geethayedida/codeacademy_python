2ff1588331decb6a903ac843f21cfa7b0218b521
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-04 10:18:44 -0700

    2-4-2016

---
+++ lambda_syntax.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 03 12:34:43 2016
+
+@author: Geetha Yedida
+"""
+
+languages = ["HTML", "JavaScript", "Python", "Ruby"]
+print filter(lambda x: x == "Python", languages)
