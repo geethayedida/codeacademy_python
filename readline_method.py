4e2e7228a9f209f453fa02894c540de68d80c102
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-05 09:34:56 -0700

    2/5/2016

---
+++ readline_method.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 04 13:43:01 2016
+
+@author: Geetha Yedida
+"""
+
+my_file = open("output.txt", "r")
+print my_file.readline()
+print my_file.readline()
+print my_file.readline()
+my_file.close()
